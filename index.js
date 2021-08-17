81678d57a454a13f09715d0e67fc684cd06b0157
janeishahu <janeishahu@aol.com>
2021-08-16 23:38:48 -0400

    set up path to display all villains and it works

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const villains = require('./villains')
 const app = express()
 
 app.get('/', (request, response) => {
   // eslint-disable-next-line no-console
   return response.send('Hello World')
 })
+
+// display all villains
+app.get('/villains', (request, response) => {
+  return response.send({ villains })
+})
+
 app.listen(1337, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port 1337')
