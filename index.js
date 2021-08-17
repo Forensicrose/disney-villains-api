17ec41958a3253d9c53fd201649f5397c244c584
janeishahu <janeishahu@aol.com>
2021-08-16 23:34:41 -0400

    tested route and it works

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (request, response) => {
+  // eslint-disable-next-line no-console
+  return response.send('Hello World')
+})
 app.listen(1337, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port 1337')
