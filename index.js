f802a9b0d59787768cb28fcf7ba384e4dd94d765
janeishahu <janeishahu@aol.com>
2021-08-16 23:50:00 -0400

    set up the get villains by slug route and the 404 error code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable eqeqeq */
 const express = require('express')
 const villains = require('./villains')
 const app = express()
@@ -11,6 +12,20 @@ app.get('/', (request, response) => {
 app.get('/villains', (request, response) => {
   return response.send({ villains })
 })
+
+// display villains by slug
+app.get('/villains/:slug', (request, response) => {
+  // deconstruct slug and any slug that comes from the request.params(from browser) // gets stored in slug; also set up a 404
+  const { slug } = request.params
+  const villainBySlug = villains.filter(villain => villain.slug == slug)
+
+  if (villainBySlug.length < 1) {
+    return response.sendStatus(404)
+  }
+
+  return response.send(villainBySlug)
+})
+
 
 app.listen(1337, () => {
   // eslint-disable-next-line no-console
