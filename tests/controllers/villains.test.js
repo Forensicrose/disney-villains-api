be98704e30f23ddf75701fb05c4e0609385de361
janeishahu <janeishahu@aol.com>
2021-08-25 22:02:51 -0400

    set up first test &  mocks folder with fake data. Ran test and it passed

--- tests/controllers/villains.test.js
+++ tests/controllers/villains.test.js
@@ -1,5 +1,29 @@
 const chai = require('chai')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
-const {describe, it} = require(mocha)
-const {getVillains, villainBySlug, newillain} = require('../../controllers/villains')
+const models = require('../../models')
+const { describe, it } = require('mocha')
+const { villainList } = require('../mocks/villains')
+const { getVillains, villainBySlug, newVillain } = require('../../controllers/villains')
+
+chai.use(sinonChai)
+const { expect } = chai
+
+describe('getting all villains', () => {
+  describe('getVillains', () => {
+    it('retrieves and returns a list of villains from the database', async () => {
+      const stubbedFindAll = sinon.stub(models.scaryVillains, 'findAll').returns(villainList)
+      const stubbedSend = sinon.stub()
+      const response = { send: stubbedSend }
+
+      await getVillains({}, response)
+
+      expect(stubbedFindAll).to.have.callCount(1)
+      expect(stubbedSend).to.have.been.calledWith(villainList)
+    })
+  })
+
+
+  describe('villainBySlug', () => {})
+  describe('newVillain', () => {})
+})
---
+++ tests/mocks/villains.js
@@ -0,0 +1,9 @@
+const villainList = [{
+  name: 'Captain Hook',
+  movie: 'Peter Pan',
+  slug: 'captain-hook',
+}]
+
+
+
+module.exports = { villainList }
