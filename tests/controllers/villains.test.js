50a876ea33fe00317efcef21bb9da080e53821e9
janeishahu <janeishahu@aol.com>
2021-08-25 21:06:41 -0400

    imported required libraries and the three controller functions to villains.test.js file

---
+++ tests/controllers/villains.test.js
@@ -0,0 +1,5 @@
+const chai = require('chai')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+const {describe, it} = require(mocha)
+const {getVillains, villainBySlug, newillain} = require('../../controllers/villains')
